df3df645f75c04380bfe37f102efbd958efaf4f1
刘浩天 <2410651402@qq.com>
2023-02-19 15:12:06 +0800

    第四次提交

--- models/init_db.go
+++ models/init_db.go
@@ -10,6 +10,7 @@ var DB *gorm.DB
 
 func InitDB() {
 	var err error
+	// gorm 进行mysql连接 返回DB+error
 	DB, err = gorm.Open(mysql.Open(config.DBConnectString()), &gorm.Config{ //建立数据库链接
 		PrepareStmt:            true, //缓存预编译命令
 		SkipDefaultTransaction: true, //禁用默认事务操作
@@ -18,6 +19,7 @@ func InitDB() {
 	if err != nil {
 		panic(err)
 	}
+	//自动根据结构体创建表 判断是否返回error
 	err = DB.AutoMigrate(&UserInfo{}, &Video{}, &Comment{}, &UserLogin{}) //初始化表  用户信息 视频 评论 用户登录
 	if err != nil {
 		panic(err)
